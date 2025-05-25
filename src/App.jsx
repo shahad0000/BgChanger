55bd23166439b9b627d95020edf80298906fe171
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-05-25 11:47:04 +0300

    change color pallete

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,13 @@ const changeColor = (color) => {
           <div className="text-5xl font-bold text-gray-700">Click a Button to change the Background</div>
         <div className="flex gap-5 justify-center">
           <button onClick={() => changeColor("bg-white")} className="bg-white p-2 px-5">Original</button>
-          <button onClick={() => changeColor("bg-[#c4ebfc]")} className="bg-[#c4ebfc] p-2 px-5">Blue</button>
-          <button onClick={() => changeColor("bg-[#dcf7c8]")} className="bg-[#dcf7c8] p-2 px-5">Green</button>
-          <button onClick={() => changeColor("bg-[#fff17a]")} className="bg-[#fff17a] p-2 px-5">Yelllow</button>
-          <button onClick={() => changeColor("bg-[#e8b0b3]")} className="bg-[#e8b0b3] p-2 px-5">Pink</button>
+          <button onClick={() => changeColor("bg-[#F0F8FF]")} className="bg-[#F0F8FF] p-2 px-5">Alice Blue</button>
+          <button onClick={() => changeColor("bg-[#E6E6FA]")} className="bg-[#E6E6FA] p-2 px-5">Lavender</button>
+          <button onClick={() => changeColor("bg-[#F5FFFA]")} className="bg-[#F5FFFA] p-2 px-5">MInt</button>
+          <button onClick={() => changeColor("bg-[#E0FFFF]")} className="bg-[#E0FFFF] p-2 px-5">Light Cyan</button>
+          <button onClick={() => changeColor("bg-[#FFDAB9]")} className="bg-[#FFDAB9] p-2 px-5">Peach</button>
+          <button onClick={() => changeColor("bg-[#FFE4E1]")} className="bg-[#FFE4E1] p-2 px-5">Rose</button>
+          <button onClick={() => changeColor("bg-[#FFFACD]")} className="bg-[#FFFACD] p-2 px-5">Lemon</button>
 
         </div>
         </div>
